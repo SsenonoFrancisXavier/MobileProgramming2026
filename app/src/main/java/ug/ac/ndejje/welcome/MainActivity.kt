3adf8e5b4d96a4131d59d517f3c22ad93dd2cc63
SsenonoFrancisXavier <ssenonofrancisxavier@gmail.com>
2026-03-28 18:19:29 +0300

    Added search bar and query or filter element. added students to the application

--- app/src/main/java/ug/ac/ndejje/welcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/welcome/MainActivity.kt
@@ -1,37 +1,49 @@
-package ug.ac.ndejje.welcome
+package ug.ac.ndejje.Welcome
 
+import android.R
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ug.ac.ndejje.welcome.Student
+import ug.ac.ndejje.welcome.StudentProvider
 import ug.ac.ndejje.welcome.ui.theme.NdejjeWelcomeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,91 +52,133 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NdejjeWelcomeAppTheme {
-                StudentIdCard()
+                StudentDirectory()
+
             }
-        }
-    }
-}
-@Composable
-fun StudentInfo() {
-    val profileImage = painterResource(R.drawable.male_student)
-    val logoImage = painterResource(R.drawable.ndu_logo)
-    Column (
-        modifier = Modifier.padding(10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Box {
-            Image(
-                painter = profileImage,
-                contentDescription = "Student Photo",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(percent = 10))
-            )
-            Image(
-                painter = logoImage,
-                contentDescription = null,
-                modifier = Modifier
-                    .size(80.dp)
-                    .align(Alignment.BottomEnd)
-                    .padding(8.dp)
-            )
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = stringResource(R.string.student_name),
-            style = MaterialTheme.typography.headlineSmall,
-            fontWeight = FontWeight.Bold,
-            color = MaterialTheme.colorScheme.primary
-        )
-        Text(
-            text = stringResource(R.string.programme),
-            style = MaterialTheme.typography.labelMedium,
-            color = MaterialTheme.colorScheme.primary
-        )
-        HorizontalDivider(
-            modifier = Modifier.padding(vertical = 12.dp, horizontal = 24.dp),
-            thickness = 1.dp,
-            color = MaterialTheme.colorScheme.outlineVariant
-        )
-        Row(){
-            Text(
-                text = "REG NO.:",
-                style = MaterialTheme.typography.labelLarge,
-                fontWeight = FontWeight.ExtraBold,
-                modifier = Modifier.padding(4.dp)
-            )
-            Spacer(modifier = Modifier.width(10.dp))
-            Text(
-                text = stringResource(R.string.reg_number),
-                style = MaterialTheme.typography.bodyLarge
-            )
         }
     }
 }
 
 @Composable
-fun StudentIdCard() {
-    ElevatedCard(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.elevatedCardColors(
-            MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f)
-        )
+fun StudentInfo(student: Student, verifiedStatus: Boolean?){
+    Column(horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        StudentInfo()
+        Image(
+            painter = painterResource(student.profileImageId),
+            contentDescription = "Profile Image",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(RoundedCornerShape(percent = 50))
+                .padding(bottom = 8.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Text(
+            text = student.name,
+            style = MaterialTheme.typography.headlineSmall
+        )
+
+        Text(
+            text = student.regNumber,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+
+
+
+        )
+
+
+        if (verifiedStatus == true) {
+            Text("Verified Student", color = Color(0xFF4CAF50))
+        } else if (verifiedStatus == false) {
+            Text("Student not verified", color = Color.Red)
+        }
+
     }
+}
+
+@Composable
+fun StudentIdCard(student: Student){
+    var showStatus by remember { mutableStateOf<Boolean?>(null) }
+
+    ElevatedCard(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
+        shape = RoundedCornerShape(size = 16.dp),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        )
+
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            StudentInfo(student,showStatus)
+            Button(onClick = {
+                showStatus = if (showStatus == null) student.isVerified else null
+            }) {
+                Text(if (showStatus == null) "View Profile" else "Hide Profile")
+            }
+        }
+    }
+}
+
+@Composable
+fun StudentDirectory(){
+    var querySearch by remember { mutableStateOf("") }
+    val allStudents = StudentProvider.studentList
+    val filteredStudent by remember (querySearch){
+        derivedStateOf {
+            if(querySearch.isBlank()){
+                allStudents
+            }
+            else {
+                allStudents.filter{
+                    it.name.contains(querySearch, ignoreCase = true)
+                }
+            }
+
+        }
+    }
+    LazyColumn(modifier = Modifier
+        .fillMaxWidth(),
+
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        item() {
+            TextField(
+                value = querySearch,
+                onValueChange = { querySearch = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                placeholder = { Text("Search Student") },
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                trailingIcon = {Icon(Icons.Default.Person, contentDescription = null)},
+                shape = RoundedCornerShape(12.dp),
+
+                )
+
+        }
+
+        items(filteredStudent){student ->
+            StudentIdCard(student = student)
+        }
+    }
+
 }
 
 @Preview(
     showBackground = true,
     showSystemUi = true
+
 )
 @Composable
 fun NdejjePreview() {
     NdejjeWelcomeAppTheme {
-        StudentIdCard()
+        StudentDirectory()
     }
 }
---
+++ app/src/main/java/ug/ac/ndejje/welcome/Student.kt
@@ -0,0 +1,10 @@
+package ug.ac.ndejje.welcome
+
+data class Student (
+    val id: Int,
+    val name: String,
+    val regNumber: String,
+    val programme: String,
+    val profileImageId: Int,
+    val isVerified: Boolean = false
+)
